205971fa19490cdf0709f0d5dd0b1232fe4a03a4
shongie <116272145+shongie@users.noreply.github.com>
2022-11-24 15:00:07 +0200

    Updates

---
+++ precache-manifest.eb327a0d36da8e6acb952d502afc15a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae09169028ecb33be247b362fbf361c",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "4f5d4225678b3dbaae0d",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b3230f7089cdea2e52c9",
+    "url": "/react-frontend-dev-portfolio/static/css/main.8a5b2c6b.chunk.css"
+  },
+  {
+    "revision": "4f5d4225678b3dbaae0d",
+    "url": "/react-frontend-dev-portfolio/static/js/2.10ea55fd.chunk.js"
+  },
+  {
+    "revision": "923a93445da4455d8e7e236ec4fa5442",
+    "url": "/react-frontend-dev-portfolio/static/js/2.10ea55fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3230f7089cdea2e52c9",
+    "url": "/react-frontend-dev-portfolio/static/js/main.1f70b225.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  },
+  {
+    "revision": "1ae29eccf51a5b38509f035f6f40ef30",
+    "url": "/react-frontend-dev-portfolio/static/media/xongi-pic.1ae29ecc.jpg"
+  }
+]);
